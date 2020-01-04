50854ee06a1e32eac05b804db80eac68c3709fb8
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-01-04 20:38:04 +0100

    add contours

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ from utils.my_image import MyImage
 images = []
 
 # init images
-for x in range(0, 6):
+for x in range(0, 1):
     file_name = 'rgb_00_00_000_0' + str(x)
     images.append(MyImage(file_name))
 
 # save images with green mask
-for img in images:
-    cv2.imwrite('./created/' + img.file_name + '.png', img.get_with_green_mask())
+# for img in images:
+#     cv2.imwrite('./created/' + img.file_name + '.png', img.get_with_green_mask())
 
+
+# display
+
+cv2.imshow('image', images[0].get_final_img())
+
+# cv2.drawContours(images[0].img, [images[0].get_biggest_proper_contour()], -1, (255, 255, 255), -1)
+# cv2.imshow('image', images[0].img)
+
+# cv2.imshow('green mask', images[0].get_inverted_green_mask())
+
+# cv2.imshow('contour mask', images[0].get_biggest_contour_mask())
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
--- utils/my_image.py
+++ utils/my_image.py
@@ -13,16 +13,82 @@ class MyImage:
 
         # import img
         self.img = cv2.imread(path)
+        self.img_w = self.img.shape[0]
+        self.img_h = self.img.shape[1]
 
         # create hsv
         self.hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
 
-    def get_with_green_mask(self):
+    def get_final_img(self):
+        # apply contour mask
+        final = cv2.bitwise_and(self.img, self.img, mask=self.get_biggest_contour_mask())
+
+        return final
+
+    def get_img_with_green_mask(self):
+        # apply green mask
+        green = cv2.bitwise_and(self.img, self.img, mask=self.get_green_mask())
+
+        return green
+
+    # GREEN
+
+    def get_green_mask(self):
         # create green mask
         green_mask = cv2.inRange(self.hsv, self.__low_green, self.__high_green)
 
-        # apply green mask
-        green = cv2.bitwise_and(self.img, self.img, mask=green_mask)
+        return green_mask
 
-        return green
+    def get_inverted_green_mask(self):
+        # create green mask
+        green_mask = cv2.inRange(self.hsv, self.__low_green, self.__high_green)
+
+        return 255 - green_mask
+
+    # CONTOURS
+
+    def get_contours(self):
+        conts, hierarchy = cv2.findContours(self.get_green_mask(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        return conts
+
+    def get_proper_contours(self):
+        proper_conts = []
+
+        for cont in self.get_contours():
+            x, y, w, h = cv2.boundingRect(cont)
+
+            if x < 5 or (x + w) > self.img_w:
+                continue
+
+            if y < 5 or (y + h) > self.img_h:
+                continue
+
+            proper_conts.append(cont)
+
+        return proper_conts
+
+    def get_biggest_proper_contour(self):
+        proper_conts = self.get_proper_contours()
+        biggest_area = 0
+        biggest_contour = proper_conts[0]
+
+        for cont in self.get_contours():
+            x, y, w, h = cv2.boundingRect(cont)
+            area = w * h
+
+            if area > biggest_area:
+                biggest_area = area
+                biggest_contour = cont
+
+        return biggest_contour
+
+    def get_biggest_contour_mask(self):
+        mask = np.zeros(self.img.shape[:2], np.uint8)
+
+        cv2.drawContours(mask, [self.get_biggest_proper_contour()], -1, (255, 255, 255), -1)
+
+        return mask
+
+
 
