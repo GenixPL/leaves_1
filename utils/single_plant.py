2312bb358bb793f9073044f2f942725dab2ffade
Łukasz Niedziałek <intergenixyt@gmail.com>
2020-01-05 00:05:30 +0100

    i give up, images move slightly and i cant distinguish new leaf

--- utils/single_plant.py
+++ utils/single_plant.py
@@ -14,18 +14,24 @@ class SinglePlant:
     ]
 
     def __init__(self, number):
-        self.__handle_first_img(0, number, 0, 0)
+        for a in range(0, 3):  # 3
+            for c in range(0, 1):  # 10
+                for d in range(0, 1):  # 6
+                    if a == 0 and c == 0 and d == 0:
+                        self.__handle_first_img(a, number, c, d)
+                    else:
+                        self.__handle_other(a, number, c, d)
 
     def __handle_first_img(self, a, b, c, d):
         file_name = '0' + str(a) + '_0' + str(b) + '_00' + str(c) + '_0' + str(d)
 
-        self.img = cv2.imread('./created/rgb_' + file_name + '.png')
+        img = cv2.imread('./created/rgb_' + file_name + '.png')
 
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        thresh, self.img_black_white = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        thresh, img_black_white = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
 
         # first conts
-        conts, hierarchy = cv2.findContours(self.img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        conts, hierarchy = cv2.findContours(img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         # getting center
         M = cv2.moments(conts[0])
@@ -33,34 +39,35 @@ class SinglePlant:
         cY = int(M["m01"] / M["m00"])
         self.center = (cX, cY)
 
-        cv2.circle(self.img_black_white, self.center, 7, (0, 0, 0), -1)
+        cv2.circle(img_black_white, self.center, 7, (0, 0, 0), -1)
 
         # another conts
-        conts2, hierarchy = cv2.findContours(self.img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        conts2, hierarchy = cv2.findContours(img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.circle(self.img, self.center, 7, self.__colors[0], -1)
+        cv2.circle(img, self.center, 7, self.__colors[0], -1)
 
         for i in range(0, len(conts2)):
-            cv2.drawContours(self.img, [conts2[i]], -1, self.__colors[i], -1)
+            cv2.drawContours(img, [conts2[i]], -1, self.__colors[i], -1)
 
-        cv2.imwrite('./colored/rgb_' + file_name + '.png', self.img)
+        self.previous = img.copy()
+        cv2.imwrite('./colored/rgb_' + file_name + '.png', img)
 
     def __handle_other(self, a, b, c, d):
         file_name = '0' + str(a) + '_0' + str(b) + '_00' + str(c) + '_0' + str(d)
+        img = cv2.imread('./created/rgb_' + file_name + '.png')
 
-        self.img = cv2.imread('./created/rgb_' + file_name + '.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        thresh, img_black_white = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
 
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        thresh, self.img_black_white = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
-
-        cv2.circle(self.img_black_white, self.center, 7, (0, 0, 0), -1)
+        cv2.circle(img_black_white, self.center, 7, (0, 0, 0), -1)
 
         # conts
-        conts2, hierarchy = cv2.findContours(self.img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        conts2, hierarchy = cv2.findContours(img_black_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.circle(self.img, self.center, 7, self.__colors[0], -1)
+        cv2.circle(img, self.center, 7, self.__colors[0], -1)
 
         for i in range(0, len(conts2)):
-            cv2.drawContours(self.img, [conts2[i]], -1, self.__colors[i], -1)
+            cv2.drawContours(img, [conts2[i]], -1, self.__colors[i % 8], -1)
 
-        cv2.imwrite('./colored/rgb_' + file_name + '.png', self.img)
+        self.previous = img.copy()
+        cv2.imwrite('./colored/rgb_' + file_name + '.png', img)
